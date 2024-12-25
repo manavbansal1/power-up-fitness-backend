3f5a30c5424c6f873188bd8e514f3a949975c9ad
Manav Bansal <bansalmanav39@gmail.com>
2024-12-24 21:15:22 -0800

    Initial commit

---
+++ src/main/java/com/project/power_up_fitness_backend/PowerUpFitnessBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.power_up_fitness_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerUpFitnessBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerUpFitnessBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/power_up_fitness_backend/PowerUpFitnessBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.power_up_fitness_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerUpFitnessBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
